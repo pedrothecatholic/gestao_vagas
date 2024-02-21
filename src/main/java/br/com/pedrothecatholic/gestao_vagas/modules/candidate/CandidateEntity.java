7f02a7e91fddeed34ebfed31c9c5f0be94124c7e
pedrothecatholic <pedro.sousa21@outlook.com>
2024-02-21 12:42:32 -0300

    Commit I de Gestão de Vagas

---
+++ src/main/java/br/com/pedrothecatholic/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,14 @@
+package br.com.pedrothecatholic.gestao_vagas.modules.candidate;
+
+import lombok.Data;
+
+import java.util.UUID;
+@Data
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String curriculum;
+}
