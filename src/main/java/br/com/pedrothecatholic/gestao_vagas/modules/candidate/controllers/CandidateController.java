360cfb9f00dcd58d9014d5673f8e86b30a1f2199
pedrothecatholic <pedro.sousa21@outlook.com>
2024-02-21 13:20:59 -0300

    Adicionda a validação de campos no cadastro de usuário

--- src/main/java/br/com/pedrothecatholic/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/pedrothecatholic/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package br.com.pedrothecatholic.gestao_vagas.modules.candidate.controllers;
 
 import br.com.pedrothecatholic.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
